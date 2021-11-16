de8c15e0d0914e88457d02cd103a7193e87e3292
Michail Chatzipanagiotou <mchatzip@c2s16d91.42wolfsburg.de>
2021-11-16 17:25:59 +0100

    added false command error

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mchatzip <mchatzip@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 11:19:39 by mchatzip          #+#    #+#             */
-/*   Updated: 2021/11/16 16:49:59 by mchatzip         ###   ########.fr       */
+/*   Updated: 2021/11/16 17:24:50 by mchatzip         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,20 @@
 void	parseargs(char *b)
 {
 	int	l;
+	int i;
 
 	l = ft_strlen(b);
+	i = -1;
 	if (!ft_strncmp(b, "ls", 2))
 		execls(b);
 	else if (!ft_strncmp(b, "pwd", l))
 		execpwd();
+	else
+	{
+		while (b[++i] != ' ' && b[i])
+			write(2, &b[i], 1);
+		write(2, ": command not found\n", 20);
+	}
 }
 
 int	main()
